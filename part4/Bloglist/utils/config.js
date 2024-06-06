9a39163eb21322fa59870da88c0708b3105d812f
inkaviita <151649923+inkaviita@users.noreply.github.com>
2024-06-06 16:22:39 +0300

    4.1-4.19 + 4.21

--- part4/Bloglist/utils/config.js
+++ part4/Bloglist/utils/config.js
@@ -2,12 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.NODE_ENV === 'test' 
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 export default {
   MONGODB_URI,
   PORT
 }
-
-
