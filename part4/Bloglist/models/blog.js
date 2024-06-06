f826446d04884d7e01f4ca12acb88fbd827f92a9
inkaviita <151649923+inkaviita@users.noreply.github.com>
2024-06-06 16:18:14 +0300

    4.1-4.19 + 4.21

--- part4/Bloglist/models/blog.js
+++ part4/Bloglist/models/blog.js
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 const blogSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true,
+        required: false,
         maxlength: 100
     },
 
     author: {
         type: String,
-        required: true,
+        required: false,
         maxlength: 100
     },
 
@@ -22,11 +22,16 @@ const blogSchema = new mongoose.Schema({
     likes: {
         type: Number,
         required: false,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     }
 })
 
 blogSchema.set('toJSON', {
     transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
         delete returnedObject._id
         delete returnedObject.__v
     }
