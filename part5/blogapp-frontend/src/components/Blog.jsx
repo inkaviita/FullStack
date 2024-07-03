9568542cbf0af19cf917df4c9cb2e727299180e3
inkaviita <151649923+inkaviita@users.noreply.github.com>
2024-07-03 15:40:38 +0300

    round 5

--- part5/blogapp-frontend/src/components/Blog.jsx
+++ part5/blogapp-frontend/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author} {blog.url}
-  </div>  
-)
+import React from "react";
+
+const Blog = ({ blog, expanded, toggleExpand, like, deleteBlog, loggedUser }) => (
+  <li className="blog">
+    {blog.title} {blog.author}
+    <p>{blog.user.username}  ---  {loggedUser.username}</p> 
+    {expanded && (
+      <><div id="moreInfo">
+        <p>URL: {blog.url}</p>
+        <div>likes: {blog.likes} <button id="likeButton" onClick={() => like(blog.id)}>like</button></div>
+        <div>{loggedUser.username}</div>
+        {blog.user.username === loggedUser.username &&(
+          <button id="delButton" onClick = {() => deleteBlog(blog.id)}>remove</button>
+        )}
+      </div>
+      </>
+    )}
+    <button id="show" onClick={toggleExpand}>
+      {expanded ? 'Show Less' : 'Show More'}
+    </button>
+  </li>
+);
 
 export default Blog
+
